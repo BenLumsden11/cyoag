23d6f383113e2738bb8a1ab3650328c101c47c98
BenLumsden11 <78027116+BenLumsden11@users.noreply.github.com>
2021-04-22 17:59:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+  
